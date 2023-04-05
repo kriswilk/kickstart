792d71e4c5f6cde7e5a810dda0f180d34747f68a
Kris Wilk <kris@reefnet.ca>
2023-04-05 17:23:48 -0400

    Create post-install.sh

---
+++ wsl/post-install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# local bin directory
+mkdir -p ~/.local/bin
+export PATH="$HOME/.local/bin:$PATH"
+
+# PPAs, updates
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt update && sudo apt upgrade -y
+
+# zsh, oh-my-zsh, p10k
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# git
+sudo apt install -y git
+
+# neovim (stable + personal config)
+git clone https://github.com/kriswilk/config-nvim.git $HOME/.config/nvim/
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+sudo apt install -y ./nvim-linux64.deb
+rm nvim-linux64.deb
+
+# direnv
+curl -sfL https://direnv.net/install.sh | bash
+
+# pip, venv, pipx
+sudo apt install -y python3-pip python3-venv
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+# pyenv (w/ build dependencies)
+curl https://pyenv.run | bash
+sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev llvm
