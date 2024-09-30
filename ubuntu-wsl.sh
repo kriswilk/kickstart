21473423148dc630c2c556fc033aa1defe040230
Kris Wilk <kris@reefnet.ca>
2024-09-30 09:53:46 -0400

    Update ubuntu-wsl.sh

--- ubuntu-wsl.sh
+++ ubuntu-wsl.sh
@@ -7,11 +7,17 @@ mkdir ~/.config
 sudo apt update && sudo apt upgrade -y
 
 # starship
+echo >> ~/.bashrc
+echo '# starship' >> ~/.bashrc
+#
 curl -sS https://starship.rs/install.sh | sh
 starship preset plain-text-symbols -o ~/.config/starship.toml
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 # uv
+echo >> ~/.bashrc
+echo '# uv' >> ~/.bashrc
+#
 curl -LsSf https://astral.sh/uv/install.sh | sh
 echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
 echo 'eval "$(uvx --generate-shell-completion bash)"' >> ~/.bashrc
