4d869f4841a193fe4f9cd61675b64af541e74225
Kris Wilk <kris@reefnet.ca>
2024-09-30 23:35:12 -0400

    Update ubuntu-wsl.sh

--- ubuntu-wsl.sh
+++ ubuntu-wsl.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# directories
-mkdir ~/.config
+# whitespace
+echo >> ~/.profile
+echo >> ~/.bashrc
 
 # updates
-sudo apt update && sudo apt upgrade -y
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y clean
+sudo apt -y autoremove
 
-# starship
-echo >> ~/.bashrc
-echo '# starship' >> ~/.bashrc
-#
-curl -sS https://starship.rs/install.sh | sh
-starship preset plain-text-symbols -o ~/.config/starship.toml
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
+# rust (unattended with "-s -- -y")
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # uv
-echo >> ~/.bashrc
-echo '# uv' >> ~/.bashrc
-#
 curl -LsSf https://astral.sh/uv/install.sh | sh
 echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
 echo 'eval "$(uvx --generate-shell-completion bash)"' >> ~/.bashrc
+
+# starship (unattended with "-s -- -y")
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+mkdir -p ~/.config
+starship preset plain-text-symbols -o ~/.config/starship.toml
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
