711d368c0fb2f0bca913228b4e527ac6b1f46252
Kris Wilk <kris@reefnet.ca>
2025-10-15 13:13:10 -0400

    WIP

--- archlinux.sh
+++ archlinux.sh
@@ -7,15 +7,15 @@ function confirm_y() { read -p "$1 [Y/n]: " ans && [[ $ans == [nN] ]] && exit 1;
 function confirm_n() { read -p "$1 [y/N]: " ans && [[ $ans == [yY] ]] || exit 1; }
 
 notify "HOSTNAME..."
-read -p "Enter the new hostname: " hostname
-if [[ ! $hostname ]]; then
+read -p "Enter the new hostname: " host
+if [[ ! $host ]]; then
   fail "ERROR: Hostname invalid."
 fi
 
 notify "TARGET DISK..."
 lsblk
 read -p "Enter the target disk: " disk
-disk="/dev/${disk}"
+disk="/dev/$disk"
 if [[ $disk == "/dev/" || ! -e $disk ]]; then
   fail "ERROR: Target disk not found."
 fi
@@ -29,25 +29,20 @@ elif ! timedatectl show | grep "NTPSynchronized=yes" &> /dev/null; then
   fail "ERROR: Clock requires synchronization." 
 fi
 
-notify "CONFIRMATION..."
-confirm_n "Disk $disk will be completely erased. Proceed?"
+notify "WIPING DISK..."
+confirm_n "Filesystem signatures on $disk will be wiped. Proceed?"
+wipefs -a $disk
 
 notify "PARTITIONING..."
-sgdisk $disk -Z -n 1:0:1G -t 1:ef00 -N 2 -t 2:8309
-if [[ -e "${disk}1" && -e "${disk}2" ]]; then
-  efi="${disk}1"
-  luks="${disk}2"
-elif [[ -e "${disk}p1" && -e "${disk}p2" ]]; then
-  efi="${disk}p1"
-  luks="${disk}p2"
-else
-  fail "ERROR: Disk partition(s) missing."
-fi
+sgdisk $disk --new 1:0:1G    --typecode 1:ef00 --change-name 1:efi \
+             --largest-new 2 --typecode 2:8309 --change-name 2:luks_system
+efi="/dev/disk/by-partlabel/efi"
+luks="/dev/disk/by-partlabel/luks_system"
 
 notify "ENCRYPTION..."
-cryptsetup luksFormat $luks
-cryptsetup open --type luks $luks archlinux
-btrfs="/dev/mapper/archlinux"
+cryptsetup luksFormat --pbkdf=pbkdf2 $luks
+cryptsetup open $luks system
+btrfs="/dev/mapper/system"
 
 notify "FORMATTING..."
 mkfs.fat -F 32 $efi
@@ -77,205 +72,64 @@ btrfs filesystem mkswapfile --size 16g --uuid clear /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
 
 notify "GENERATING MIRRORLIST..."
-reflector --country CA --delay 1 --fastest 10 --sort rate --save /etc/pacman.d/mirrorlist --verbose
+reflector --country CA \
+          --delay 1 \
+          --fastest 10 \
+          --sort rate \
+          --save /etc/pacman.d/mirrorlist \
+          --verbose
 
 ## WIP: update packages first?
-notify "INSTALLING PACKAGES..."
-packages=(
-  # CORE PACKAGES, DRIVERS
-  # minimal
-  base
-  base-devel
-  linux
-  linux-firmware
-  linux-headers
-  # microcode
-  amd-ucode
-  intel-ucode
-  # bootloader
-  grub
-  grub-btrfs
-  efibootmgr
-  os-prober
-  # filesystem tools
-  btrfs-progs
-  dosfstools
-  exfatprogs
-  e2fsprogs
-  ntfs-3g
-  udftools
-  ##### mtools gparted gptfdisk ???????????????????????
-  # network
-  networkmanager
-  iwd
-  ufw
-  tailscale
-  wireshark-cli
-  wireshark-gui
-  qbittorrent
-  
-  # sound
-  sof-firmware # ??????
-  alsa-firmware # ??????
-  pipewire # ??????
-  pipewire-alsa # ??????
-  pipewire-pulse # ??????
-  pipewire-jack # ??????
-  wireplumber # ??????
-  easyeffects # ??????
-  alsa-utils # ??????
-
-  
-  # basic tools
-  git rsync rclone htop reflector tmux fastfetch
-  
-  # fonts
-  ttf-hack-nerd
-  
-  # 
-  firefox # browser
-  nano vim # editor
-  rust uv # programming
-  docker # virtualization
-  
-  # MULTIMEDIA
-  # audio
-  audacity
-  # photo / illustration
-  gimp
-  inkscape
-  krita
-  darktable
-  flameshot
-  # video
-  vlc
-  mkvtoolnix-cli
-  mkvtoolnix-gui
-  # 3d / cad
-  blender
-  freecad
-  kicad
-  prusa-slicer
-  # office
-  libreoffice
-  
-  # ENTERTAINMENT
-  # gaming
-  steam
-  prismlauncher
-  jre8-openjdk
-  jre17-openjdk
-  jre21-openjdk
-
-  # shell
-  # gui
-  # terminal
-  
-# AUR LIST
-stremio
-naps2
-bcompare
-bcompare-kde6 ?????????
-visual-studio-code-bin
-imhex
-hexcurse
-ipscan
-rustdesk
-pandoc-bin
-
-# openssh man 
-# bash-completion
-    # fonts
-    # onedrive, gdrive
-    # fineprint
-    # renamer
-    # flatpak?
-    # segger j-link
-    # jdownloader2
-    # rufus
-
-    
-    nvidia nvidia-settings
-    deluge meld speedcrunch okteta
-    wine winetricks wine-mono wine-gecko
-    
-    gvim mousepad # For when neovim doesn't like me.
-    python tk python-pyperclip
-    wl-clipboard
-    
-    gvfs
-
-    # Install KDE.
-    plasma
-    xorg
-    kdialog
-    konsole # Terminal.
-    kate # Text editor i never use. NOTE: This also installs kwrite.
-    dolphin dolphin-plugins kio-admin
-    ark p7zip unrar # Archive management tools.
-    kcharselect # Character selector.
-    kcalc # Calculator.
-    gwenview # Image viewer.
-    kcolorchooser # Color picker.
-    filelight # Disk space usage.
-    spectacle # Screenshot capture.
-    okular # PDF and comic viewer.
-    gparted # Gnomes "GParted" is better (included with manjaro kde btw) than "KDE Partition Manager": partitionmanager
-    gnome-disk-utility # Gnomes other partition manager, that has a handy "Restore Disk Image..." feature that i use to burn iso's to usb instead of using $ dd command.
-    ksystemlog # System log viewer.
-    gsmartcontrol # Harddisk health inspector.
-    plasma-systemmonitor # Task Manager.
-    plasma-desktop plasma-nm
-
-    kwallet kwalletmanager # Needed by KDE to encrypt credentials so they aren't plain text.
-    kwallet-pam # Allows for auto-unlock once configured.
-    libsecret # Allows GTK applications (like Chrome) to interface with KDE Wallet.
-
-    # gnome-keyring # Only needed if you use GNOME apps that rely on the GNOME keyring.
-    # seahorse # GUI for GNOME keyring.
-
-    # kaccounts-integration # For online account management (Google, Nextcloud, OwnCloud, etc.) in KDE.
-
-    egl-wayland plasma-wayland-protocols # Wayland.
-
-    sddm # Display manager.
-)
-pacstrap -K /mnt "${packages[@]}"
-## WIP: will amd/intel microcode coexist??
+notify "INSTALLING ESSENTIAL PACKAGES..."
+pacstrap -K /mnt base linux linux-firmware \
+                 amd-ucode intel-ucode \
+                 btrfs-progs dosfstools \
+                 networkmanager iwd openssh \
+                 grub efibootmgr
 
 notify "GENERATING FSTAB..."
 genfstab -U /mnt > /mnt/etc/fstab
-## WIP: need to remove subvolid from fstab?? or is it not put in there anymore?
 
 notify "SETTING TIME ZONE..."
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 arch-chroot /mnt hwclock --systohc
 
 notify "SETTING LOCALE..."
-echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
 notify "SETTING HOSTNAME..."
-echo $hostname > /mnt/etc/hostname
+echo $host > /mnt/etc/hostname
 
-notify "RECREATING THE INITRAMFS IMAGE..."
+notify "CONFIGURE & REBUILD INITRAMFS..."
 sed -i "s/block filesystems/block encrypt filesystems/" /mnt/etc/mkinitcpio.conf
 arch-chroot /mnt mkinitcpio -P
 
+notify "INSTALL BOOTLOADER..."
+sed -i "/GRUB_CMDLINE_LINUX=/c\GRUB_CMDLINE_LINUX=\"cryptdevice=${luks}:luks_system\"" /mnt/etc/default/grub
+sed -i "/GRUB_ENABLE_CRYPTODISK=/c\GRUB_ENABLE_CRYPTODISK=y" /mnt/etc/default/grub
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
 notify "CONFIGURE USERS..."
 arch-chroot /mnt passwd
-# WIP create other users
+# WIP create regular users
 
-notify "INSTALL BOOTLOADER..."
-## WIP: this is systemd-boot...change to grub for snapshots?
-#arch-chroot /mnt bootctl install
+notify "CONFIGURE NETWORKING..."
+cat > /mnt/etc/NetworkManager/conf.d/wifi_backend.conf << EOF
+[device]
+wifi.backend=iwd
+EOF
 
 notify "ENABLE SERVICES..."
+arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt systemctl enable sshd
 ## WIP.....
 
 notify "REBOOT..."
 confirm_y "Ready to reboot. Proceed?"
+swapoff -a
 umount -R /mnt
-cryptsetup close $luks
+cryptsetup close system
 reboot
